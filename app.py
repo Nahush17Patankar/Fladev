12c35715efd2f01af05440228f4a6a5c8a59c264
Nahush Patankar <nahushpatankar17@gmail.com>
2026-04-01 10:55:22 +0530

    db issue fixed

--- app.py
+++ app.py
@@ -1,20 +1,39 @@
-from flask import Flask
+# from flask import Flask
+# import mysql.connector
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     try:
+#         db = mysql.connector.connect(
+#             host="db",
+#             user="root",
+#             password="root",
+#             database="testdb"
+#         )
+#         return "Connected to MySQL successfully!"
+#     except:
+#         return "Database connection failed!"
+
+# if __name__ == "__main__":
+#     app.run(host="localhost", port=5000)
+
+
+
+import time
 import mysql.connector
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
+for i in range(10):
     try:
-        db = mysql.connector.connect(
+        conn = mysql.connector.connect(
             host="db",
             user="root",
             password="root",
             database="testdb"
         )
-        return "Connected to MySQL successfully!"
+        print("Connected to DB!")
+        break
     except:
-        return "Database connection failed!"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+        print("Retrying DB connection...")
+        time.sleep(5)
